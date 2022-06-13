aedcbb63dac826b90408a3a3bfc36e051f828752
Obifrankie <obifranklina@gmal.com>
2022-06-13 12:47:12 +0100

    stable commit

---
+++ client/src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'c9cabf9d-d14e-4022-aca8-eb934725b4b2',
+    redirector: 'http://localhost:3000',
+    scopes: [
+        'user.read'
+    ],
+    authority: 'https://login.microsoftonline.com/softwareDiningAd.onmicrosoft.com'
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = 'mongodb://softwarediningdb:4sVyZGPH1xk68SnhoZXNkN55Jq5DAISUrN8FFFxjkH4Yr72O2wjE770PNxZBtMdJkp1BHwqRzpIgGFRANQSEOg==@softwarediningdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@softwarediningdb@';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
